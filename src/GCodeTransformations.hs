889d2680b963a76c7bca82c134f8c2e70a1548c8
Michael Lotkowski <mllotkow@amazon.com>
2022-08-25 14:25:32 +0100

    Scale IJ params

--- src/GCodeTransformations.hs
+++ src/GCodeTransformations.hs
@@ -1,6 +1,6 @@
 module GCodeTransformations (toOrigin, setOutputSize) where
 
-import Data.GCode (updateLimitsCode, AxisDesignator (X, Y), modifyXY)
+import Data.GCode (updateLimitsCode, AxisDesignator (X, Y), modifyXY, modifyParam, ParamDesignator (I, J))
 import Data.GCode.Types (GCode)
 import qualified Data.Map as M
 
@@ -14,18 +14,20 @@ setOutputSize :: (Maybe Double, Maybe Double) -> GCode -> GCode
 setOutputSize (Nothing, Nothing) code = code
 setOutputSize outputSize code = do
   let ((_, xmax), (_, ymax)) = findLimits code
-  let mov x y = case outputSize of
+  let (xScale, yScale) = case outputSize of
         (Just xSize, Nothing) -> do
           let scale = xSize / xmax
-          (x * scale, y * scale)
+          (scale, scale)
         (Nothing, Just ySize) -> do
           let scale = ySize / ymax
-          (x * scale, y * scale)
+          (scale, scale)
         (Just xSize, Just ySize) -> do
-          let xScale = xSize / xmax
-          let yScale = ySize / ymax
-          (x * xScale, y * yScale)
-  map (modifyXY mov) code
+          let _xScale = xSize / xmax
+          let _yScale = ySize / ymax
+          (_xScale, _yScale)
+  let mov x y = (x * xScale, y * yScale)
+
+  map (modifyParam I (* xScale) . modifyParam J (* yScale). modifyXY mov) code
 
 findLimits :: GCode -> ((Double, Double), (Double, Double))
 findLimits code = case (M.lookup X lim, M.lookup Y lim) of
